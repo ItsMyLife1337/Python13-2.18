c38f62b33ca899d6fc14083a557360b82c45ad2c
ItsMyLife1337 <itsmylife1337@yandex.ru>
2022-12-17 00:21:51 +0300

    Добавление индивидуального задания №2

---
+++ Individuals/Individual2.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import argparse
+from dotenv import load_dotenv
+import os
+import sys
+
+
+def add_student(students, name, group, grade):
+    students.append(
+        {
+            'name': name,
+            'group': group,
+            'grade': grade,
+        }
+    )
+    return students
+
+
+def show_list(students):
+    # Заголовок таблицы.
+    if students:
+
+        line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+            '-' * 4,
+            '-' * 30,
+            '-' * 20,
+            '-' * 15
+        )
+        print(line)
+        print(
+            '| {:^4} | {:^30} | {:^20} | {:^15} |'.format(
+                "№",
+                "Ф.И.О.",
+                "Группа",
+                "Успеваемость"
+            )
+        )
+        print(line)
+
+        # Вывести данные о всех студентах.
+        for idx, student in enumerate(students, 1):
+            print(
+                '| {:>4} | {:<30} | {:<20} | {:>15} |'.format(
+                    idx,
+                    student.get('name', ''),
+                    student.get('group', ''),
+                    student.get('grade', 0)
+                )
+            )
+        print(line)
+    else:
+        print("Список студентов пуст.")
+
+
+def show_selected(students):
+    # Инициализировать счетчик.
+    line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+        '-' * 4,
+        '-' * 30,
+        '-' * 20,
+        '-' * 15
+    )
+    print(line)
+    print(
+        '| {:^4} | {:^30} | {:^20} | {:^15} |'.format(
+            "№",
+            "Ф.И.О.",
+            "Группа",
+            "Успеваемость"
+        )
+    )
+    print(line)
+    count = 0
+    # Проверить сведения студентов из списка.
+    for student in students:
+        grade = [int(x) for x in (student.get('grade', '').split())]
+        if sum(grade) / max(len(grade), 1) >= 4.0:
+            count += 1
+            print(
+                '| {:>4} | {:<30} | {:<20} | {:>15} |'.format(
+                    count,
+                    student.get('name', ''),
+                    student.get('group', ''),
+                    student.get('grade', 0)
+                )
+            )
+    print(line)
+    if count == 0:
+        print("Студенты с баллом 4.0 и выше не найдены.")
+
+
+def help_1():
+    print("Список команд:\n")
+    print("add - добавить студента;")
+    print("display - вывести список студентов;")
+    print("select - запросить студентов с баллом выше 4.0;")
+    print("save - сохранить список студентов;")
+    print("load - загрузить список студентов;")
+    print("exit - завершить работу с программой.")
+
+
+def save_students(file_name, students):
+    with open(file_name, "w", encoding="utf-8") as fout:
+        json.dump(students, fout, ensure_ascii=False, indent=4)
+
+
+def load_students(file_name):
+    with open(file_name, "r", encoding="utf-8") as fin:
+        loaded = json.load(fin)
+    return loaded
+
+
+def main(command_line=None):
+    # Создать родительский парсер для определения имени файла.
+    file_parser = argparse.ArgumentParser(add_help=False)
+    file_parser.add_argument(
+        "-d",
+        "--data",
+        action="store",
+        help="The data file name"
+    )
+
+    # Создать основной парсер командной строки.
+    parser = argparse.ArgumentParser("students")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s 0.1.0"
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Создать субпарсер для добавления студента.
+    add = subparsers.add_parser(
+        "add",
+        parents=[file_parser],
+        help="Add a new student"
+    )
+    add.add_argument(
+        "-n",
+        "--name",
+        action="store",
+        required=True,
+        help="The student's name"
+    )
+    add.add_argument(
+        "-g",
+        "--group",
+        type=int,
+        action="store",
+        help="The student's group"
+    )
+    add.add_argument(
+        "-gr",
+        "--grade",
+        action="store",
+        required=True,
+        help="The student's grade"
+    )
+
+    # Создать субпарсер для отображения всех студентов.
+    _ = subparsers.add_parser(
+        "display",
+        parents=[file_parser],
+        help="Display all students"
+    )
+
+    # Создать субпарсер для выбора студентов.
+    select = subparsers.add_parser(
+        "select",
+        parents=[file_parser],
+        help="Select the students"
+    )
+    select.add_argument(
+        "-s",
+        "--select",
+        action="store",
+        required=True,
+        help="The required select"
+    )
+
+    # Выполнить разбор аргументов командной строки.
+    args = parser.parse_args(command_line)
+
+    data_file = args.data
+    dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    if not data_file:
+        data_file = os.getenv("STUDENTS_DATA")
+    if not data_file:
+        print("The data file name is absent", file=sys.stderr)
+        sys.exit(1)
+
+    # Загрузить всех студентов из файла, если файл существует.
+    is_dirty = False
+    if os.path.exists(data_file):
+        students = load_students(data_file)
+    else:
+        students = []
+
+    # Добавить студента.
+    if args.command == "add":
+        students = add_student(
+            students,
+            args.name,
+            args.group,
+            args.grade
+        )
+        is_dirty = True
+    # Отобразить всех студентов.
+    elif args.command == "display":
+        show_list(students)
+    # Выбрать требуемых студентов.
+    elif args.command == "select":
+        show_selected(students)
+
+    # Сохранить данные в файл, если список студентов был изменен.
+    if is_dirty:
+        save_students(data_file, students)
+
+
+if __name__ == '__main__':
+    main()
